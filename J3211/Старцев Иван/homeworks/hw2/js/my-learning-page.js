f485dda7649da739efe28ddf33fa656aeec598d3
Ivan Startsev <aladush3k@mail.ru>
2026-03-22 23:54:02 +0300

    feat: улучшены теги на странице "мое обучение"

--- J3211/Старцев Иван/homeworks/hw2/js/my-learning-page.js
+++ J3211/Старцев Иван/homeworks/hw2/js/my-learning-page.js
@@ -15,20 +15,20 @@
 
         emptyState.classList.toggle("d-none", filteredCourses.length > 0);
         document.getElementById("learningCoursesContainer").innerHTML = filteredCourses.map((course) => `
-            <div class="col-12 col-md-6 col-xl-4">
-                <div class="card h-100">
+            <li class="col-12 col-md-6 col-xl-4">
+                <article class="card h-100">
                     <img src="${course.image}" class="card-img-top" alt="${course.title}">
                     <div class="card-body d-flex flex-column">
                         <h5 class="card-title">${course.title}</h5>
                         <p class="card-text text-muted small">${course.description}</p>
-                        <p class="card-text mb-2"><i class="bi bi-star-fill rating-star"></i> ${getCourseRating(course).toFixed(1)} / 5</p>
+                        <p class="card-text mb-2"><i class="bi bi-star-fill rating-star" aria-hidden="true"></i> ${getCourseRating(course).toFixed(1)} / 5</p>
                         <div class="mt-auto">
                             <a href="lesson.html?id=${course.id}" class="btn btn-success btn-sm">Продолжить</a>
                             <a href="course.html?id=${course.id}" class="btn btn-outline-primary btn-sm ms-1">О курсе</a>
                         </div>
                     </div>
-                </div>
-            </div>
+                </article>
+            </li>
         `).join("");
     };
 
