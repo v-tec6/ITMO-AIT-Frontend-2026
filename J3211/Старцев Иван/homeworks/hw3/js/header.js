2cec82ce7925cf6b2d8aaaed6b7577a42c337272
Ivan Startsev <aladush3k@mail.ru>
2026-03-23 03:26:59 +0300

    feat: реализована смена темы

--- J3211/Старцев Иван/homeworks/hw3/js/header.js
+++ J3211/Старцев Иван/homeworks/hw3/js/header.js
@@ -21,7 +21,7 @@
         const user = await window.auth.loadCurrentUser();
 
         header.innerHTML = `
-            <nav class="navbar navbar-expand-lg bg-dark navbar-dark">
+            <nav class="navbar navbar-expand-lg bg-dark navbar-dark border-bottom">
                 <div class="container-fluid">
                     <a class="navbar-brand" href="courses.html">Старцев Курсы</a>
 
@@ -31,24 +31,30 @@
                         <span class="navbar-toggler-icon"></span>
                     </button>
 
-                    ${user ? `
-                        <div class="dropdown order-lg-last ms-2">
-                            <button class="btn p-0" type="button" data-bs-toggle="dropdown" data-bs-auto-close="true" data-bs-display="static" aria-expanded="false" aria-label="открыть меню профиля">
-                                <img src="${user.avatar}" alt="${user.name}" class="rounded-circle object-fit-cover" width="40" height="40">
-                            </button>
-                            <ul class="dropdown-menu dropdown-menu-end shadow-sm rounded-3 p-2">
-                                <li><a class="dropdown-item rounded-2 px-3 py-2" href="profile.html"> Профиль</a></li>
-                                <li><a class="dropdown-item rounded-2 px-3 py-2" href="my-learning.html">Моё обучение</a></li>
-                                <li><a class="dropdown-item rounded-2 px-3 py-2" href="my-courses.html"> Мои курсы</a></li>
-                                <li><hr class="dropdown-divider"></li>
-                                <li><button class="dropdown-item rounded-2 px-3 py-2 text-danger" type="button" data-logout> Выйти</button></li>
-                            </ul>
-                        </div>
-                    ` : `
-                        <a class="btn btn-outline-light order-lg-last ms-2" href="login.html" aria-label="Войти">
-                            <i class="bi bi-door-open" aria-hidden="true"></i>
-                        </a>
-                    `}
+                    <div class="d-flex align-items-center order-lg-last gap-2">
+                        <button class="btn btn-outline-secondary theme-toggle-btn rounded-circle" type="button" data-theme-toggle aria-label="Переключить тему">
+                            <i class="bi bi-circle-half" aria-hidden="true"></i>
+                        </button>
+
+                        ${user ? `
+                            <div class="dropdown">
+                                <button class="btn p-0" type="button" data-bs-toggle="dropdown" data-bs-auto-close="true" data-bs-display="static" aria-expanded="false" aria-label="открыть меню профиля">
+                                    <img src="${user.avatar}" alt="${user.name}" class="rounded-circle object-fit-cover" width="40" height="40">
+                                </button>
+                                <ul class="dropdown-menu dropdown-menu-end shadow-sm rounded-3 p-2">
+                                    <li><a class="dropdown-item rounded-2 px-3 py-2" href="profile.html">Профиль</a></li>
+                                    <li><a class="dropdown-item rounded-2 px-3 py-2" href="my-learning.html">Моё обучение</a></li>
+                                    <li><a class="dropdown-item rounded-2 px-3 py-2" href="my-courses.html">Мои курсы</a></li>
+                                    <li><hr class="dropdown-divider"></li>
+                                    <li><button class="dropdown-item rounded-2 px-3 py-2 text-danger" type="button" data-logout>Выйти</button></li>
+                                </ul>
+                            </div>
+                        ` : `
+                            <a class="btn btn-outline-light" href="login.html" aria-label="Войти">
+                                <i class="bi bi-door-open" aria-hidden="true"></i>
+                            </a>
+                        `}
+                    </div>
 
                     <div class="collapse navbar-collapse" id="mainNavCollapse">
                         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
@@ -65,6 +71,12 @@
             logoutButton.onclick = () => {
                 window.auth.clearSession();
                 window.location.replace("courses.html");
+            };
+        }
+        const themeButton = header.querySelector("[data-theme-toggle]");
+        if (themeButton) {
+            themeButton.onclick = () => {
+                window.theme.toggle();
             };
         }
     };
