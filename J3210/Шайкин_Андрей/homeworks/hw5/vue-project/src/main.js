2fc8ef9612f31bb0190bbf659ca76b8eeda661eb
Kekendr <ashaikin00@mail.ru>
2026-04-17 20:00:50 +0300

    feat: setup application entry points and global state management

--- J3210/Шайкин_Андрей/homeworks/hw5/vue-project/src/main.js
+++ J3210/Шайкин_Андрей/homeworks/hw5/vue-project/src/main.js
@@ -1,12 +1,16 @@
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
 
-import App from './App.vue'
-import router from './router'
+import App from '@/App.vue'
+import router from '@/router'
+import store from '@/stores'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap'
+import '@/assets/main.css'
 
 const app = createApp(App)
 
-app.use(createPinia())
+app.use(store)
 app.use(router)
 
 app.mount('#app')
