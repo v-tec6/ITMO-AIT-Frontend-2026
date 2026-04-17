389bdd56c8be97d53b1d6c3e38a7b85af4e47c2b
Kekendr <ashaikin00@mail.ru>
2026-04-17 20:08:48 +0300

    feat: setup router and basic ui components markup

--- J3210/Шайкин_Андрей/homeworks/hw5/vue-project/src/router/index.js
+++ J3210/Шайкин_Андрей/homeworks/hw5/vue-project/src/router/index.js
@@ -2,7 +2,13 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'notes',
+      component: () => import('../views/NotesPage.vue')
+    }
+  ]
 })
 
 export default router
